c1ebefbb2adfceebcef271fdaad1b52dd6dc04bf
arun kannan <sarun@co.anteriad.com>
2023-08-06 22:04:18 +0900

    Added files

---
+++ src/main/java/com/slin/app/ms01/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.slin.app.ms01.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+	@GetMapping("/get")
+	public String get () {
+		return "Home";
+	}
+}
---
+++ src/test/java/com/slin/app/ms01/Ms01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slin.app.ms01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
